4f6f1672250b5f215ae2d89fdaa9d9a0880dc20c
jawaldi348 <jawaldi416@gmail.com>
2021-09-05 13:00:13 +0700

    beri hak akses ke controller welcome jika belum login

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,6 +3,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Welcome extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		userlogin();
+	}
 	public function index()
 	{
 		$data = [
