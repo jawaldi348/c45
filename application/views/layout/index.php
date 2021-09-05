998adc4c61068125ccef1b3ba2c6e84480b13074
jawaldi348 <jawaldi416@gmail.com>
2021-09-05 12:27:44 +0700

    templating halaman home

---
+++ application/views/layout/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Decision Tree C4.5</title>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="stylesheet" href="<?= assets() ?>bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= assets() ?>bower_components/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?= assets() ?>bower_components/icomoon/styles.css">
+    <link rel="stylesheet" href="<?= assets() ?>dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="<?= assets() ?>dist/css/skins/_all-skins.min.css">
+    <script src="<?= assets() ?>bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="<?= assets() ?>bower_components/jquery-ui/jquery-ui.min.js"></script>
+    <script>
+        $.widget.bridge('uibutton', $.ui.button);
+    </script>
+    <script src="<?= assets() ?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="<?= assets() ?>bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="<?= assets() ?>bower_components/fastclick/lib/fastclick.js"></script>
+    <script src="<?= assets() ?>dist/js/adminlte.min.js"></script>
+    <script src="<?= assets() ?>dist/js/pages/dashboard.js"></script>
+    <script src="<?= assets() ?>dist/js/demo.js"></script>
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+        <header class="main-header">
+            <a href="<?= base_url() ?>" class="logo">
+                <span class="logo-mini">Decision Tree</span>
+                <span class="logo-lg"><b>Decision Tree</b></span>
+            </a>
+            <nav class="navbar navbar-static-top">
+                <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                    <span class="sr-only">Toggle navigation</span>
+                </a>
+                <div class="navbar-custom-menu">
+                    <ul class="nav navbar-nav">
+                        <li>
+                            <a href="#"><i class="fa fa-power-off"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <?php $this->load->view('layout/sidebar') ?>
+        <div class="content-wrapper">
+            <section class="content-header">
+                <h1>{title}</h1>
+                <ol class="breadcrumb">
+                    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                </ol>
+            </section>
+            <section class="content">
+                {content}
+            </section>
+        </div>
+        <footer class="main-footer">
+            <strong>Decision Tree C4.5</strong>
+        </footer>
+    </div>
+</body>
+
+</html>
---
+++ application/views/layout/sidebar.php
@@ -0,0 +1,9 @@
+<aside class="main-sidebar">
+    <section class="sidebar">
+        <ul class="sidebar-menu" data-widget="tree">
+            <li>
+                <a href="<?= site_url('welcome') ?>"><i class="fa fa-dashboard"></i> <span>Beranda</span></a>
+            </li>
+        </ul>
+    </section>
+</aside>
