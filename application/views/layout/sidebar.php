24d012b9c23804d2efdbb5b19ebdeed777330da9
jawaldi348 <jawaldi416@gmail.com>
2021-09-05 12:28:36 +0700

    mengaktifkan menu aktif pada sidebar

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -1,7 +1,8 @@
+<?php $urls = $this->uri->segment(1); ?>
 <aside class="main-sidebar">
     <section class="sidebar">
         <ul class="sidebar-menu" data-widget="tree">
-            <li>
+            <li class="<?= $urls == "welcome" ? "active" : null ?>">
                 <a href="<?= site_url('welcome') ?>"><i class="fa fa-dashboard"></i> <span>Beranda</span></a>
             </li>
         </ul>
