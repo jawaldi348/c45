07a024e0d859d35d9865a6cb1c63e63b18ec7e5a
jawaldi348 <jawaldi416@gmail.com>
2021-09-07 16:28:36 +0700

    upload file excel

--- application/controllers/Data.php
+++ application/controllers/Data.php
@@ -77,6 +77,27 @@ class Data extends CI_Controller
     {
         $this->load->view('data/upload');
     }
+    public function upload_file($filename)
+    {
+        $this->load->library('upload'); // Load librari upload
+
+        $config['upload_path'] = './excel/';
+        $config['allowed_types'] = 'xlsx';
+        $config['max_size']  = '2048';
+        $config['overwrite'] = true;
+        $config['file_name'] = $filename;
+
+        $this->upload->initialize($config); // Load konfigurasi uploadnya
+        if ($this->upload->do_upload('file')) { // Lakukan upload dan Cek jika proses upload berhasil
+            // Jika berhasil :
+            $return = array('result' => 'success', 'file' => $this->upload->data(), 'error' => '');
+            return $return;
+        } else {
+            // Jika gagal :
+            $return = array('result' => 'failed', 'file' => '', 'error' => $this->upload->display_errors());
+            return $return;
+        }
+    }
 }
 
 /* End of file Data.php */
